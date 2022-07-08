d07dbf2417038b1347384a9901dcf5f600102433
ms29ash <ms29ash@gmail.com>
2022-07-08 13:37:25 +0530

    add endpoint querying course with id

--- app.js
+++ app.js
@@ -21,16 +21,34 @@ const port = process.env.PORT || 4000;
 app.get('/', (req, res) => {
     res.send('Apni Kaksha')
 })
-app.get('/', (req, res) => {
-    res.send('Apni Kaksha')
+
+
+app.get('/course/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        // const views = req.params.views;
+        // const id = '62c7d56fc7d48436060ff413'
+        console.log(id)
+
+        let course = await Course.find({ _id: id.toString() });
+        // let course = await Course.findOne({ views: views });
+        res.json(course);
+    } catch (error) {
+        console.log(error);
+    }
+
 })
+// app.post('/course', async (req, res) => {
+//     let user = await Course.create(req.body);
+//     res.json(user)
 
+// })
 
-app.get('/course', async (req, res) => {
-    let user = await Course.find();
-    res.json(user)
+// app.get('/course', async (req, res) => {
+//     let course = await Course.find();
+//     res.json(course)
 
-})
+// })
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
